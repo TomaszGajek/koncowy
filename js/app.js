a77f0097f06589029f1a19c296a2a289fee5ef3e
tomaszgajek <kosmagajek@gmail.com>
2017-01-30 09:33:45 +0100

    projects view

--- js/app.js
+++ js/app.js
@@ -1,109 +1,122 @@
 var app = angular.module("app",['ngRoute']);
 
-app.config(function($routeProvider,$locationProvider){
+/* -- ROUTER -- */
 
-	$locationProvider.hashPrefix('');
+	app.config(function($routeProvider,$locationProvider){
 
-	$routeProvider
-		.when('/',{
-			templateUrl:'pages/main.html',
-			controller:'mainController'
-		})
-		.when('/about',{
-			templateUrl:'pages/about.html',
-			controller:'secondController'
-		})
-		.when('/projects',{
-			templateUrl:'pages/projects.html',
-			controller:'thirdController'
-		}).
-		when('/contact',{
-			templateUrl:'pages/contact.html',
-			controller:'fourthController'
-		})
+		$locationProvider.hashPrefix('');
 
-
-});
-
-
-app.directive('navigation',function(){
-
-	return {
-		template:
-				'<div id="navToggle" ng-click="slide()">'
-					+'<span></span>'
-				+'</div>'
-				+'<ul class="navigation">'
-		        	+'<li><a href="#">home</a></li>'
-		        	+'<li><a href="#/about">about</a></li>'
-		        	+'<li><a href="#/projects">projects</a></li>'
-		        	+'<li><a href="#/contact">contact</a></li>'
-			    +'</ul>'
-			    
-	}
-
-});
-
-app.controller('mainController',['$scope','myService',function($scope,myService){
-
-	$scope.slide = myService.slide;
-
-}]);
+		$routeProvider
+			.when('/',{
+				templateUrl:'pages/main.html',
+				controller:'mainController'
+			})
+			.when('/about',{
+				templateUrl:'pages/about.html',
+				controller:'secondController'
+			})
+			.when('/projects',{
+				templateUrl:'pages/projects.html',
+				controller:'thirdController'
+			}).
+			when('/contact',{
+				templateUrl:'pages/contact.html',
+				controller:'fourthController'
+			})
 
 
-app.controller('secondController',['$scope','myService',function($scope, myService){
+	});
+/* -- END of ROUTER -- */
 
-	$scope.slide = myService.slide;
+/* -- TEMPLATES -- */
 
-}]);
+	app.directive('navigation',function(){
 
-app.controller('thirdController',['$scope','myService',function($scope, myService){
+		return {
+			template:
+					'<div id="navToggle" ng-click="slide()">'
+						+'<span></span>'
+					+'</div>'
+					+'<ul class="navigation">'
+			        	+'<li><a href="#">home</a></li>'
+			        	+'<li><a href="#/about">about</a></li>'
+			        	+'<li><a href="#/projects">projects</a></li>'
+			        	+'<li><a href="#/contact">contact</a></li>'
+				    +'</ul>'
+				    
+		}
 
-	$scope.slide = myService.slide;
+	});
+/* -- END of TEMPLATES -- */
 
-	$scope.projects = [
+/* -- CONTROLLERS -- */
 
-		{
-			img: 'img/project1.jpg'
-		},
-		{
-			img: 'img/project2.jpg'
-		},
-		{
-			img: 'img/project3.jpg'
+	app.controller('mainController',['$scope','myService',function($scope,myService){
+
+		$scope.slide = myService.slide;
+
+	}]);
+
+
+	app.controller('secondController',['$scope','myService',function($scope, myService){
+
+		$scope.slide = myService.slide;
+
+	}]);
+
+	app.controller('thirdController',['$scope','myService',function($scope, myService){
+
+		$scope.slide = myService.slide;
+
+		$scope.projects = [
+
+			{
+				img: 'img/project1.jpg',
+				href:'http://www.tomaszgajek.pl'
+			},
+			{
+				img: 'img/project2.jpg',
+				href:'https://tomaszgajek.github.io/innox/'
+			},
+			{
+				img: 'img/project3.jpg',
+				href:'https://tomaszgajek.github.io/Bhagaskara/'
+			}
+
+
+		]
+
+
+	}]);
+
+
+	app.controller('fourthController',['$scope','myService',function($scope, myService){
+
+		$scope.slide = myService.slide;
+
+
+
+
+	}]);
+/* -- END of CONTROLLERS -- */
+
+/* -- SERVICE -- */
+
+	app.service('myService',function(){
+
+		var slideNav = function(){
+			$('.navigation').slideToggle();
 		}
 
 
-	]
+		return {
 
+			slide: slideNav
 
-}]);
+		}
 
-
-app.controller('fourthController',['$scope','myService',function($scope, myService){
-
-	$scope.slide = myService.slide;
-
-
-
-
-}]);
-
-
-app.service('myService',function(){
-
-	var slideNav = function(){
-		$('.navigation').slideToggle();
-	}
-
-
-	return {
-
-		slide: slideNav
-
-	}
-
-});
+	});
+/* -- END of SERVICE -- */
 
 
 
